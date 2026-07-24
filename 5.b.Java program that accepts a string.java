a7cb291cb2682e380d969fdf26c5c0b75122f89d
shruthiofficial29-creator <shruthiofficial29@gmail.com>
2026-07-24 14:19:31 +0530

    Create 5.b.Java program that accepts a string.java

---
+++ 5.b.Java program that accepts a string.java
@@ -0,0 +1,36 @@
+import java.util.Scanner;
+
+public class CountCharacters {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String text = sc.nextLine();
+        int vowels = 0;
+        int consonants = 0;
+        int digits = 0;
+        int specialChars = 0;
+        for (int i = 0; i < text.length(); i++) {
+            char ch = text.charAt(i);
+            if (Character.isLetter(ch)) {
+                char lowerCh = Character.toLowerCase(ch);
+                if (lowerCh == 'a' || lowerCh == 'e' || lowerCh == 'i' || lowerCh == 'o' || lowerCh == 'u') {
+                    vowels++;
+                } else {
+                    consonants++;
+                }
+            } 
+            else if (Character.isDigit(ch)) {
+                digits++;
+            }
+            else if (!Character.isWhitespace(ch)) {
+                specialChars++;
+            }
+        }
+        System.out.println("Vowels: " + vowels);
+        System.out.println("Consonants: " + consonants);
+        System.out.println("Digits: " + digits);
+        System.out.println("Special Characters: " + specialChars);
+
+        sc.close();
+    }
+}

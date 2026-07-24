0d5e1c457d7c01e6b56632f6db6cb183211d6c70
Shruthi S <shruthiofficial29@gmail.com>
2026-07-24 19:59:03 +0530

    Update EX: 3(c)-Given an array of coordinates, check whether these points make a straight line in the XY plane (LeetCode 1232 – Check If It Is a Straight Line).java

--- EX: 3(c)-Given an array of coordinates, check whether these points make a straight line in the XY plane (LeetCode 1232 – Check If It Is a Straight Line).java
+++ EX: 3(c)-Given an array of coordinates, check whether these points make a straight line in the XY plane (LeetCode 1232 – Check If It Is a Straight Line).java
@@ -1,3 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        int[][] coordinates1 = {{1, 2}, {2, 3}, {3, 4}, {4, 5}, {5, 6}, {6, 7}};
+        boolean result1 = solution.checkStraightLine(coordinates1);
+        System.out.println("Test Case 1 Output: " + result1);
+       
+    }
+}
 class Solution {
     public boolean checkStraightLine(int[][] coordinates) {
         int x0 = coordinates[0][0], y0 = coordinates[0][1];

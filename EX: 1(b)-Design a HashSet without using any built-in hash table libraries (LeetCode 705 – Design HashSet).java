c55cb61c4af0cb551d9d7346abfc170860f6f9f6
Shruthi S <shruthiofficial29@gmail.com>
2026-07-24 19:45:43 +0530

    Update EX: 1(b)-Design a HashSet without using any built-in hash table libraries (LeetCode 705 – Design HashSet).java

--- EX: 1(b)-Design a HashSet without using any built-in hash table libraries (LeetCode 705 – Design HashSet).java
+++ EX: 1(b)-Design a HashSet without using any built-in hash table libraries (LeetCode 705 – Design HashSet).java
@@ -1,17 +1,37 @@
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+    public static void main(String[] args) {
+        MyHashSet myHashSet = new MyHashSet();
+        List<Object> output = new ArrayList<>();
+        output.add(null);
+        myHashSet.add(1);
+        output.add(null);
+        myHashSet.add(2);
+        output.add(null);
+        output.add(myHashSet.contains(1));
+        output.add(myHashSet.contains(3));
+        myHashSet.add(2);
+        output.add(null);
+        output.add(myHashSet.contains(2));
+        myHashSet.remove(2);
+        output.add(null);
+        output.add(myHashSet.contains(2));
+        System.out.println(output);
+    }
+}
 class MyHashSet {
     private boolean[] set;
-
     public MyHashSet() {
         set = new boolean[1000001];
     }
     public void add(int key) {
         set[key] = true;
     }
-    
     public void remove(int key) {
         set[key] = false;
     }
-    
     public boolean contains(int key) {
         return set[key];
     }

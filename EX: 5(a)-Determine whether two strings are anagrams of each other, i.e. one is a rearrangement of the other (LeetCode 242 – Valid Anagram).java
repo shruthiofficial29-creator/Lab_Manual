7ef3f7292b1b1350b027b1f9d03b77b461024c78
Shruthi S <shruthiofficial29@gmail.com>
2026-07-24 20:05:53 +0530

    Update EX: 5(a)-Determine whether two strings are anagrams of each other, i.e. one is a rearrangement of the other (LeetCode 242 – Valid Anagram).java

--- EX: 5(a)-Determine whether two strings are anagrams of each other, i.e. one is a rearrangement of the other (LeetCode 242 – Valid Anagram).java
+++ EX: 5(a)-Determine whether two strings are anagrams of each other, i.e. one is a rearrangement of the other (LeetCode 242 – Valid Anagram).java
@@ -1,3 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+        Solution solution = new Solution();
+        String s = "anagram";
+        String t = "nagaram";
+
+        boolean result = solution.isAnagram(s, t);
+        System.out.println("Output: " + result);
+    }
+}
+
 class Solution {
     public boolean isAnagram(String s, String t) {
         if (s.length() != t.length()) return false;

38667af771b3b6ca50814df7b99bc469f56aa388
Shruthi S <shruthiofficial29@gmail.com>
2026-07-24 19:43:32 +0530

    Update EX 1(a)-Design a parking system for a parking lot with a fixed number of spaces for big, medium, and small cars (LeetCode 1603 – Design Parking System).java

--- EX 1(a)-Design a parking system for a parking lot with a fixed number of spaces for big, medium, and small cars (LeetCode 1603 – Design Parking System).java
+++ EX 1(a)-Design a parking system for a parking lot with a fixed number of spaces for big, medium, and small cars (LeetCode 1603 – Design Parking System).java
@@ -1,8 +1,26 @@
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) {
+        ParkingSystem parkingSystem = new ParkingSystem(1, 1, 0);
+
+        boolean[] results = new boolean[4];
+        results[0] = parkingSystem.addCar(1);
+        results[1] = parkingSystem.addCar(2);
+        results[2] = parkingSystem.addCar(3);
+        results[3] = parkingSystem.addCar(1);
+
+        System.out.println(Arrays.toString(results));
+    }
+}
+
 class ParkingSystem {
     private int[] count;
+
     public ParkingSystem(int big, int medium, int small) {
-                count = new int[]{0, big, medium, small};
+        count = new int[]{0, big, medium, small};
     }
+
     public boolean addCar(int carType) {
         return count[carType]-- > 0;
     }
